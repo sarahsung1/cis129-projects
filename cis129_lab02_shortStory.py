b8d6ca4ac3a1eded78316f5b9c0aade078189447
sarahsung1 <158612713+sarahsung1@users.noreply.github.com>
2024-02-04 14:13:06 -0700

    Update cis129_lab02_shortStory.py
    short story

--- cis129_lab02_shortStory.py
+++ cis129_lab02_shortStory.py
@@ -1,3 +1,8 @@
 # Author: Sarah Sung
 # My first Python lab in my CIS129 course
 # Prints a simple short story
+print('The fuzzy cat lay quietly asleep on the soft blanket.')
+print('The cat was abruptly awaken by the birds chirping that stalked outside.')
+print('The cat watched the window intensely, staying still.')
+print('As a bird flew by, the cat jumped up.')
+print('The whole flock of birds flew hurriedly away.')
